835b1e9b759d5df163aaf510eaa5dedc4a8270e4
myz <zmy_00@sjtu.edu.cn>
2024-11-24 14:01:07 +0800

    version1

---
+++ build/kernel/CMakeFiles/kernel.img.dir/DependInfo.cmake
@@ -0,0 +1,80 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/root/OS-Course-Lab/Lab2/build/kernel/incbin_procmgr_bin.S" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/incbin_procmgr_bin.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "CHCORE"
+  "CHCORE_ARCH=\"aarch64\""
+  "CHCORE_ARCH_AARCH64"
+  "CHCORE_ASLR"
+  "CHCORE_CROSS_COMPILE=\"aarch64-linux-gnu-\""
+  "CHCORE_KERNEL_ENABLE_QEMU_VIRTIO_NET"
+  "CHCORE_KERNEL_PM_USAGE_TEST"
+  "CHCORE_PLAT=\"raspi3\""
+  "CHCORE_PLAT_RASPI3"
+  "CHCORE_SUBPLAT=\"\""
+  "LOG_LEVEL=1"
+  "__ASM__"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/root/OS-Course-Lab/Lab2/kernel/include"
+  "/root/OS-Course-Lab/Lab2/kernel/user-include"
+  "/root/OS-Course-Lab/Lab2/kernel/include/arch/aarch64"
+  "/root/OS-Course-Lab/Lab2/kernel/include/arch/aarch64/plat/raspi3"
+  "/root/OS-Course-Lab/Lab2/kernel/arch/aarch64/boot/raspi3/include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/OS-Course-Lab/Lab2/kernel/arch/aarch64/irq/pgfault.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/arch/aarch64/irq/pgfault.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/arch/aarch64/main.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/arch/aarch64/main.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/arch/aarch64/mm/page_table.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/arch/aarch64/mm/page_table.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/lib/rbtree.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/lib/rbtree.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/mm/buddy.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/mm/buddy.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/mm/kmalloc.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/mm/kmalloc.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/mm/mm.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/mm/mm.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/mm/pgfault_handler.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/mm/pgfault_handler.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/mm/slab.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/mm/slab.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/mm/vmspace.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/mm/vmspace.c.obj"
+  "/root/OS-Course-Lab/Lab2/kernel/tests/runtime/tests.c" "/root/OS-Course-Lab/Lab2/build/kernel/CMakeFiles/kernel.img.dir/tests/runtime/tests.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CHCORE"
+  "CHCORE_ARCH=\"aarch64\""
+  "CHCORE_ARCH_AARCH64"
+  "CHCORE_ASLR"
+  "CHCORE_CROSS_COMPILE=\"aarch64-linux-gnu-\""
+  "CHCORE_KERNEL_ENABLE_QEMU_VIRTIO_NET"
+  "CHCORE_KERNEL_PM_USAGE_TEST"
+  "CHCORE_PLAT=\"raspi3\""
+  "CHCORE_PLAT_RASPI3"
+  "CHCORE_SUBPLAT=\"\""
+  "LOG_LEVEL=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/root/OS-Course-Lab/Lab2/kernel/include"
+  "/root/OS-Course-Lab/Lab2/kernel/user-include"
+  "/root/OS-Course-Lab/Lab2/kernel/include/arch/aarch64"
+  "/root/OS-Course-Lab/Lab2/kernel/include/arch/aarch64/plat/raspi3"
+  "/root/OS-Course-Lab/Lab2/kernel/arch/aarch64/boot/raspi3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/kernel/CMakeFiles/kernel.img.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kernel.img.dir/arch/aarch64/irq/pgfault.c.obj"
+  "CMakeFiles/kernel.img.dir/arch/aarch64/main.c.obj"
+  "CMakeFiles/kernel.img.dir/arch/aarch64/mm/page_table.c.obj"
+  "CMakeFiles/kernel.img.dir/incbin_procmgr_bin.S.obj"
+  "CMakeFiles/kernel.img.dir/lib/rbtree.c.obj"
+  "CMakeFiles/kernel.img.dir/mm/buddy.c.obj"
+  "CMakeFiles/kernel.img.dir/mm/kmalloc.c.obj"
+  "CMakeFiles/kernel.img.dir/mm/mm.c.obj"
+  "CMakeFiles/kernel.img.dir/mm/pgfault_handler.c.obj"
+  "CMakeFiles/kernel.img.dir/mm/slab.c.obj"
+  "CMakeFiles/kernel.img.dir/mm/vmspace.c.obj"
+  "CMakeFiles/kernel.img.dir/tests/runtime/tests.c.obj"
+  "kernel.img"
+  "kernel.img.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/kernel.img.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
